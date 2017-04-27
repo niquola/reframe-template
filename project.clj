39644aadaf21ee7d42eb369f3cc8741449033d41
niquola <niquola@gmail.com>
2017-04-27 13:17:35 +0300

    fix cljs build

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.495" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.521" :scope "provided"]
                  [cljsjs/react-with-addons "15.4.2-2"]
                  [reagent "0.6.1" :exclusions [cljsjs/react]]
                  [re-frame "0.9.2" :exclusions [cljsjs/react]]
